7761edfa70f041716fa336fd83862574fbefe7dc
Restyled.io <commits@restyled.io>
2021-03-19 07:17:59 +0000

    Restyled by autopep8

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
+import platform
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-import platform
 
 app = Flask(__name__)
 
-my_system = platform.uname() 
+my_system = platform.uname()
 print(f"System: {my_system.system}")
 print(f"Node Name: {my_system.node}")
 print(f"Release: {my_system.release}")
@@ -15,18 +15,21 @@ print(f"Version: {my_system.version}")
 print(f"Machine: {my_system.machine}")
 print(f"Processor: {my_system.processor}")
 
+
 @app.route('/')
 def render_static():
-    return render_template('index.html', input_from_python = os.getenv('API_KEY'))
-  
+    return render_template('index.html', input_from_python=os.getenv('API_KEY'))
+
 
 @app.route('/success')
 def success():
     return render_template('success.html')
 
+
 @app.route('/canceled')
 def canceled():
     return render_template('canceled.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
