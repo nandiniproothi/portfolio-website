0b418c5f79ef5fe82c91fac6e8717e811fe73dae
Glitch (nandiniproothi) <none>
2021-03-22 17:55:12 +0000

    🌮👚 Updated with Glitch

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
-import os
-import platform
-
+from flask import Flask, render_template
 from dotenv import load_dotenv
-from flask import Flask
-from flask import render_template
+import os
 
 load_dotenv()
+import platform
 
 app = Flask(__name__)
 
-my_system = platform.uname()
+my_system = platform.uname() 
 print(f"System: {my_system.system}")
 print(f"Node Name: {my_system.node}")
 print(f"Release: {my_system.release}")
@@ -17,22 +15,18 @@ print(f"Version: {my_system.version}")
 print(f"Machine: {my_system.machine}")
 print(f"Processor: {my_system.processor}")
 
-
-@app.route("/")
+@app.route('/')
 def render_static():
-    return render_template("index.html",
-                           input_from_python=os.getenv("API_KEY"))
+    return render_template('index.html', input_from_python = os.getenv('API_KEY'))
+  
 
-
-@app.route("/success")
+@app.route('/success')
 def success():
-    return render_template("success.html")
+    return render_template('success.html')
 
-
-@app.route("/canceled")
+@app.route('/canceled')
 def canceled():
-    return render_template("canceled.html")
+    return render_template('canceled.html')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
